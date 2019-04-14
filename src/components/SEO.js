61b29002331b532d58fa994107c123cb74a6d8d6
Pol Milian <polcius@gmail.com>
2019-04-14 18:14:13 +0800

    favicons

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-import favicon from '../../static/favicon.png';
+import faviconApple from '../../static/apple-touch-icon.png';
+import favicon16 from '../../static/favicon-16x16.png';
+import favicon32 from '../../static/favicon-32x32.png';
+import faviconSafari from '../../static/safari-pinned-tab.svg';
 
 const SEO = props => (
   <StaticQuery
@@ -16,7 +19,10 @@ const SEO = props => (
           title={title}
           titleTemplate={`%s - ${data.site.siteMetadata.title}`}
           link={[
-            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
+            { rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png', href: `${faviconApple}` },
+            { rel: 'icon', sizes: '32x32', type: 'image/png', href: `${favicon32}` },
+            { rel: 'icon', sizes: '16x16', type: 'image/png', href: `${favicon16}` },
+            { rel: 'mask-icon', color: "#5bbad5", href: `${faviconSafari}` }
           ]}
         />
       );
