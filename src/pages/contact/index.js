b0c2ed1e788516c1e4b5a45a35949277ebfa6a2e
Pol Milian <polcius@gmail.com>
2019-04-15 21:16:10 +0800

    adapted to needs

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -11,63 +11,18 @@ const Contact = props => (
         <div className="row">
           <div className="col-12">
             <h1>Contact</h1>
+            <p>If you think you are a responsible farmer, join us!</p>
           </div>
         </div>
       </div>
     </div>
     <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <Call button={false} />
+      <div className="row mb-5">
+        <div className="col-12 mb-2">
+          <Call button={false} phone="9841506201" name="Sanjeev Dhakal" />
         </div>
-        <div className="col-8">
-          <h4 className="mt-4">Business Hours</h4>
-          <table className="table table-sm opening-hours-table">
-            <tbody>
-              <tr>
-                <td className="day font-weight-bold">Monday</td>
-                <td className="opens">8:30am</td>
-                <td>-</td>
-                <td className="closes">5:00pm</td>
-              </tr>
-              <tr>
-                <td className="day font-weight-bold">Tuesday</td>
-                <td className="opens">8:30am</td>
-                <td>-</td>
-                <td className="closes">5:00pm</td>
-              </tr>
-              <tr>
-                <td className="day font-weight-bold">Wednesday</td>
-                <td className="opens">8:30am</td>
-                <td>-</td>
-                <td className="closes">5:00pm</td>
-              </tr>
-              <tr>
-                <td className="day font-weight-bold">Thursday</td>
-                <td className="opens">8:30am</td>
-                <td>-</td>
-                <td className="closes">5:00pm</td>
-              </tr>
-              <tr>
-                <td className="day font-weight-bold">Friday</td>
-                <td className="opens">8:30am</td>
-                <td>-</td>
-                <td className="closes">5:00pm</td>
-              </tr>
-              <tr>
-                <td className="day font-weight-bold">Saturday</td>
-                <td className="opens">Closed</td>
-                <td />
-                <td className="closes" />
-              </tr>
-              <tr>
-                <td className="day font-weight-bold">Sunday</td>
-                <td className="opens">Closed</td>
-                <td />
-                <td className="closes" />
-              </tr>
-            </tbody>
-          </table>
+        <div className="col-12">
+          <Call button={false} phone="9849826142" name="Dhurba Regmi" />
         </div>
       </div>
     </div>
