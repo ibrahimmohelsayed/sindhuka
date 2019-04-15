a26f2fc42e425ff94b58ead8cb4f08fe1cdd956f
Pol Milian <polcius@gmail.com>
2019-04-15 21:13:54 +0800

    fixed links, removed unused data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,15 @@ module.exports = {
     description: `Join SINDHUKA’s sustainable farmers’ network: an added value for your business, a concrete support to local economy. 
     Sindhuka is a trademark which connects local vegetables, fruits and herbal plants producers aligned with
     sustainable and environmentally friendly production models.`,
-    contact: {
-      phone: '9 841 506 201',
-      email: 'Sanjeev Dhakal',
-    },
+
     menuLinks: [
       {
         name: 'Get started',
-        link: '/get-started',
+        link: '/#get-started',
       },
       {
         name: 'Locations',
-        link: '/locations',
+        link: '/#locations',
       },
       {
         name: 'Contact',
