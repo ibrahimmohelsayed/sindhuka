dae833643d4b8f3c733c4987782c3869c097eb73
Pol Milian <polcius@gmail.com>
2019-04-15 21:14:06 +0800

    unneeded pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,37 +24,11 @@ exports.createPages = ({ graphql, actions }) => {
                 }
               }
             }
-            locations: allMarkdownRemark(
-              filter: { fileAbsolutePath: { regex: "/locations/" } }
-              sort: { fields: [frontmatter___date], order: DESC }
-            ) {
-              edges {
-                node {
-                  id
-                  frontmatter {
-                    path
-                    title
-                    date(formatString: "DD MMMM YYYY")
-                  }
-                  excerpt
-                }
-              }
-            }
           }
         `
       ).then(result => {
         result.data.services.edges.forEach(({ node }) => {
           const component = path.resolve('src/templates/service.js');
-          createPage({
-            path: node.frontmatter.path,
-            component,
-            context: {
-              id: node.id,
-            },
-          });
-        });
-        result.data.locations.edges.forEach(({ node }) => {
-          const component = path.resolve('src/templates/location.js');
           createPage({
             path: node.frontmatter.path,
             component,
