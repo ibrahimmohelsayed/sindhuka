9c5cbb98e61c443335c2db522b0891195be97b38
Pol Milian <polcius@gmail.com>
2019-04-24 15:56:48 +0930

    useTranslations example

--- src/components/useTranslations.js
+++ src/components/useTranslations.js
@@ -30,6 +30,7 @@ const query = graphql`
           name
           translations: childTranslationsJson {
             hello
+            responsibleFarmer
           }
         }
       }
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import SEO from '../../components/SEO';
 import Call from '../../components/Call';
+import useTranslations from '../../components/useTranslations';
 
-const Contact = props => (
-  <>
-    <SEO title="Contact" />
-    <div className="intro intro-small">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <h1>Contact</h1>
-            <p>If you think you are a responsible farmer, join us!</p>
+const Contact = props => {
+  const { responsibleFarmer } = useTranslations();
+  return (
+    <>
+      <SEO title="Contact" />
+      <div className="intro intro-small">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1>Contact</h1>
+              <p>{responsibleFarmer}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="container">
-      <div className="row mb-5">
-        <div className="col-12 mb-2">
-          <Call button={false} phone="9841506201" name="Sanjeev Dhakal" />
-        </div>
-        <div className="col-12">
-          <Call button={false} phone="9849826142" name="Dhurba Regmi" />
+      <div className="container">
+        <div className="row mb-5">
+          <div className="col-12 mb-2">
+            <Call button={false} phone="9841506201" name="Sanjeev Dhakal" />
+          </div>
+          <div className="col-12">
+            <Call button={false} phone="9849826142" name="Dhurba Regmi" />
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
-
+    </>
+  );
+};
 export default Contact;
