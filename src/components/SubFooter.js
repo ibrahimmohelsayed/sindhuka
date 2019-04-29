574fd0d203f6327fd3d869dca3f5d91d53e37aed
Pol Milian <polcius@gmail.com>
2019-04-29 16:51:08 +0930

    finished

--- src/components/SubFooter.js
+++ src/components/SubFooter.js
@@ -1,30 +1,55 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import LocalizedLink from './LocalizedLink';
+import useTranslations from './useTranslations';
 
-const SubFooter = props => (
-  <div className="sub-footer-strip">
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="sub-footer">
-            <ul>
-              <li>
-                {' '}
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                {' '}
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li className="copyright">
-                © {new Date().getFullYear()} Sindhuka
-              </li>
-            </ul>
+const SubFooter = props => {
+  const { home, locations, contact } = useTranslations();
+  const menuLinks = [
+    {
+      name: `${home}`,
+      link: '/#get-started',
+    },
+    {
+      name: `${locations}`,
+      link: '/#locations',
+    },
+    {
+      name: `${contact}`,
+      link: '/#contact',
+    },
+  ];
+  return (
+    <div className="sub-footer-strip">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="sub-footer">
+              <ul>
+                {menuLinks.map(link => (
+                  <li key={link.name}>
+                    <LocalizedLink to={link.link}>{link.name}</LocalizedLink>
+                  </li>
+                ))}
+              </ul>
+              <ul>
+                <li>
+                  <a href="mailto:pol.milian.dev@gmail.com">
+                    Made by <strong>Pol Milian</strong>
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li className="copyright">
+                  © {new Date().getFullYear()} Sindhuka
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SubFooter;
