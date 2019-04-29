e5afa93f6250954cda318bbdd73929747562f640
Pol Milian <polcius@gmail.com>
2019-04-29 13:22:54 +0930

    links and alt for images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ import ASIALogo from '../images/logos/ASIA_logo.jpg';
 import CEAPREDLogo from '../images/logos/CEAPRED_logo.png';
 import OIKOSLogo from '../images/logos/OIKOS_logo.png';
 import PuntoSudLogo from '../images/logos/PuntoSud_logo.jpg';
+import NepalLogo from '../images/logos/nepal_logo.png';
 
 const Footer = props => {
   const { disclaimer } = useTranslations();
@@ -19,11 +20,27 @@ const Footer = props => {
               <div className="col-12">
                 <p className="footer-title py-3">{disclaimer}</p>
                 <div className="footer-logos">
-                  <img src={AICSLogo} />
-                  <img src={ASIALogo} />
-                  <img src={CEAPREDLogo} />
-                  <img src={OIKOSLogo} />
-                  <img src={PuntoSudLogo} />
+                  <a href="https://www.aics.gov.it/language/en/">
+                    <img src={AICSLogo} alt="AICS Logo" />
+                  </a>
+                  <a href="https://asia-ngo.org/">
+                    <img src={ASIALogo} alt="Asia Onlus Logo" />
+                  </a>
+                  <a href="https://www.ceapred.org.np/">
+                    <img src={CEAPREDLogo} alt="CEAPRED Logo" />
+                  </a>
+                  <a href="https://www.istituto-oikos.org/">
+                    <img src={OIKOSLogo} alt="Instituto Oikos Logo" />
+                  </a>
+                  <a href="http://www.puntosud.org/">
+                    <img src={PuntoSudLogo} alt="Punto Sud Logo" />
+                  </a>
+                  <a href="http://kalimatimarket.gov.np/home/">
+                    <img
+                      src={NepalLogo}
+                      alt=" Government of Nepal, Ministry of Agriculture and Livestock Development Logo"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
