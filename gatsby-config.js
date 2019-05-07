e137f187c824f5f4b5b5c982ea9f611a84814e34
Pol Milian <polcius@gmail.com>
2019-05-07 13:11:58 +0930

    fix build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: 'apple-touch-icon.png', // This path is relative to the root of the site.
+        icon: './static/apple-touch-icon.png', // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
